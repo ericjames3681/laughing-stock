b1e4a44862bb737597719bb8dc8318dc01069638
apple <apple@MacBook-4.local>
2020-03-02 13:52:25 -0600

    Materializing more...

--- controllers/venues.js
+++ controllers/venues.js
@@ -1,20 +1,32 @@
 const Venue = require('../models/venue');
 
 module.exports = {
+    // show,
     new: newVenue,
-    create
+    create,
+
 };
 
 function newVenue(req, res) {
-    res.render('venues/new');
+    res.render('venues/new', {
+    user: req.user}
+    );
 }
-
+function index(req, res) {
+    res.render('index', {
+        user: req.user
+    });
+}
 function create(req, res) {
     req.body.twoDrink = !!req.body.twoDrink;
     const venue = new Venue(req.body);
     venue.save(function(err, doc) {
-        if(err) return res.render('venues/new');
+        if(err) return res.render('venues/new', {venues, user: req.user});
         console.log(venue);
         res.redirect('/venues/new');
     });
 }
+
+// function show(req, res) {
+//     res.redirect('venues/show');
+// }
--- routes/venues.js
+++ routes/venues.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const venuesCtrl = require('../controllers/venues');
 /* GET /venues/new . */
 router.get('/new', venuesCtrl.new);
+// router.get('/', venuesCtrl.index);
 router.post('/', venuesCtrl.create);
 
 // router.get( '/users', usersCtrl.index);
